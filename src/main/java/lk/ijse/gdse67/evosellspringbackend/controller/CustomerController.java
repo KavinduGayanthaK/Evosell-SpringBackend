858798219c3aff263e2bc47f3875c2e2a468bafa
Kavindu <gayanthakavindu3@gmail.com>
2024-10-09 00:25:47 +0530

    Created CustomerController

---
+++ src/main/java/lk/ijse/gdse67/evosellspringbackend/controller/CustomerController.java
@@ -0,0 +1,9 @@
+package lk.ijse.gdse67.evosellspringbackend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/custo")
+public class CustomerController {
+}

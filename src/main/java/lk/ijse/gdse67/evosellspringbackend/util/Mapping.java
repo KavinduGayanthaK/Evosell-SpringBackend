31c9252810d498927d49bd565f25228a62e792be
Kavindu <gayanthakavindu3@gmail.com>
2024-10-08 16:16:17 +0530

    Created UserEntityList convert to UserDtoList

--- src/main/java/lk/ijse/gdse67/evosellspringbackend/util/Mapping.java
+++ src/main/java/lk/ijse/gdse67/evosellspringbackend/util/Mapping.java
@@ -3,8 +3,11 @@ package lk.ijse.gdse67.evosellspringbackend.util;
 import lk.ijse.gdse67.evosellspringbackend.dto.UserDTO;
 import lk.ijse.gdse67.evosellspringbackend.entity.UserEntity;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class Mapping {
@@ -18,6 +21,10 @@ public class Mapping {
 
     public UserDTO toUSerDto(UserEntity userEntity) {
         return modelMapper.map(userEntity, UserDTO.class);
+    }
+
+    public List<UserDTO> toUserDtoList(List<UserEntity> userEntities) {
+        return modelMapper.map(userEntities,new TypeToken<List<UserDTO>>() {}.getType());
     }
 
 

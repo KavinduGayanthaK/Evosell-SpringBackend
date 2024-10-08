bae81d8ca1c8245fc180d7e2a555bb0a3b94db4e
Kavindu <gayanthakavindu3@gmail.com>
2024-10-08 15:51:02 +0530

    Created HealthCheckController

---
+++ src/main/java/lk/ijse/gdse67/evosellspringbackend/controller/HealthCheckController.java
@@ -0,0 +1,17 @@
+package lk.ijse.gdse67.evosellspringbackend.controller;
+
+import org.hibernate.engine.spi.ManagedEntity;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/health")
+public class HealthCheckController {
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public String healthTest() {
+        return "Health test is working";
+    }
+}

ec000e8995e543e1f59bb963dc58b397d4d7dbda
Kavindu <gayanthakavindu3@gmail.com>
2024-10-08 15:25:29 +0530

    Added lombok annotation

--- src/main/java/lk/ijse/gdse67/evosellspringbackend/entity/UserEntity.java
+++ src/main/java/lk/ijse/gdse67/evosellspringbackend/entity/UserEntity.java
@@ -2,9 +2,17 @@ package lk.ijse.gdse67.evosellspringbackend.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Entity
-public class UserEntity {
+@Table(name = "user")
+public class UserEntity implements SuperEntity{
     @Id
     private String userId;
     private String userName;

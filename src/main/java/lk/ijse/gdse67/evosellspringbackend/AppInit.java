6f72c6622b1e55401013b8362d9e2f91b64ba30b
Kavindu <gayanthakavindu3@gmail.com>
2024-10-08 13:07:56 +0530

    Configuration in AppInit

--- src/main/java/lk/ijse/gdse67/evosellspringbackend/AppInit.java
+++ src/main/java/lk/ijse/gdse67/evosellspringbackend/AppInit.java
@@ -1,20 +1,22 @@
 package lk.ijse.gdse67.evosellspringbackend;
 
+import lk.ijse.gdse67.evosellspringbackend.config.WebAppConfig;
+import lk.ijse.gdse67.evosellspringbackend.config.WebAppRootConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class AppInit  extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
+        return new Class[]{WebAppRootConfig.class};
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[0];
+        return new Class[]{WebAppConfig.class};
     }
 
     @Override
     protected String[] getServletMappings() {
-        return new String[0];
+        return new String[]{"/"};
     }
 }

8ecc9b6df3aa4eeddfbd62b22991df1e4953fb03
Kavindu <gayanthakavindu3@gmail.com>
2024-10-08 15:57:19 +0530

    Created generateUserId

--- src/main/java/lk/ijse/gdse67/evosellspringbackend/util/AppUtil.java
+++ src/main/java/lk/ijse/gdse67/evosellspringbackend/util/AppUtil.java
@@ -3,5 +3,7 @@ package lk.ijse.gdse67.evosellspringbackend.util;
 import java.util.UUID;
 
 public class AppUtil {
-
+    public static String generateUserid() {
+        return "USER-"+UUID.randomUUID();
+    }
 }

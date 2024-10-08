7e6445b793eef65bcc361bad99d9678e49d3d82f
Kavindu <gayanthakavindu3@gmail.com>
2024-10-08 22:45:00 +0530

    Added UserNotFoundException

--- src/main/java/lk/ijse/gdse67/evosellspringbackend/exception/UserNotFoundException.java
+++ src/main/java/lk/ijse/gdse67/evosellspringbackend/exception/UserNotFoundException.java
@@ -1,5 +1,15 @@
 package lk.ijse.gdse67.evosellspringbackend.exception;
 
 public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException() {
+        super();
+    }
 
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }

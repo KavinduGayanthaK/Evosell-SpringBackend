eea3a23e949494898172f74771bc9aa2e3980ff3
Kavindu <gayanthakavindu3@gmail.com>
2024-10-08 13:54:51 +0530

    UserDao was extended form jpaRepository

--- src/main/java/lk/ijse/gdse67/evosellspringbackend/dao/UserDao.java
+++ src/main/java/lk/ijse/gdse67/evosellspringbackend/dao/UserDao.java
@@ -1,4 +1,7 @@
 package lk.ijse.gdse67.evosellspringbackend.dao;
 
-public interface UserDao {
+import lk.ijse.gdse67.evosellspringbackend.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDao extends JpaRepository<UserEntity,String> {
 }

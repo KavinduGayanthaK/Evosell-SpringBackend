39d5d089d78cd569d3bcf4bfe6a554c8538401b5
Kavindu <gayanthakavindu3@gmail.com>
2024-10-11 13:32:48 +0530

    Modifying delete user

--- src/main/java/lk/ijse/gdse67/evosellspringbackend/dao/UserDao.java
+++ src/main/java/lk/ijse/gdse67/evosellspringbackend/dao/UserDao.java
@@ -2,8 +2,10 @@ package lk.ijse.gdse67.evosellspringbackend.dao;
 
 import lk.ijse.gdse67.evosellspringbackend.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -12,6 +14,8 @@ public interface UserDao extends JpaRepository<UserEntity,String> {
     @Query(value = "SELECT u FROM UserEntity u WHERE u.gmail =?1")
     Optional<UserEntity> findByGmail(String gmail);
 
+    @Modifying
+    @Transactional
     @Query(value = "DELETE FROM UserEntity u WHERE u.gmail = ?1")
-    Optional<UserEntity> deleteByUser(String gmail);
+    void deleteByUser(String gmail);
 }

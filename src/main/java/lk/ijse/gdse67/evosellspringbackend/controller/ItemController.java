704f2b51d57b0ae072bbde3e7b4a5c338148cdb4
Kavindu <gayanthakavindu3@gmail.com>
2024-10-11 18:34:06 +0530

    Created ItemController

---
+++ src/main/java/lk/ijse/gdse67/evosellspringbackend/controller/ItemController.java
@@ -0,0 +1,9 @@
+package lk.ijse.gdse67.evosellspringbackend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "api/v1/items")
+public class ItemController {
+}

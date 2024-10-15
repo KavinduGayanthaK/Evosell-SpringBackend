55eaa9d8f92ce7b0c60e8ac004f20c236ed777e7
Kavindu <gayanthakavindu3@gmail.com>
2024-10-15 23:00:59 +0530

    Added annotation

--- src/main/java/lk/ijse/gdse67/evosellspringbackend/service/impl/OrderServiceImpl.java
+++ src/main/java/lk/ijse/gdse67/evosellspringbackend/service/impl/OrderServiceImpl.java
@@ -1,6 +1,10 @@
 package lk.ijse.gdse67.evosellspringbackend.service.impl;
 
 import lk.ijse.gdse67.evosellspringbackend.service.OrderService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional
 public class OrderServiceImpl implements OrderService {
 }

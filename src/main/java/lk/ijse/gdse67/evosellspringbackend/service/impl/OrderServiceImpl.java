61fae2b931cd28c2f27e37ae7baf722b14ddfd98
Kavindu <gayanthakavindu3@gmail.com>
2024-10-15 22:35:49 +0530

    Created OrderService

---
+++ src/main/java/lk/ijse/gdse67/evosellspringbackend/service/impl/OrderServiceImpl.java
@@ -0,0 +1,6 @@
+package lk.ijse.gdse67.evosellspringbackend.service.impl;
+
+import lk.ijse.gdse67.evosellspringbackend.service.OrderService;
+
+public class OrderServiceImpl implements OrderService {
+}

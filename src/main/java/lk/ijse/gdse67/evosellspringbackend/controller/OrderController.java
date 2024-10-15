523d944ea8dfc51b49eee77295d3e41803505ad8
Kavindu <gayanthakavindu3@gmail.com>
2024-10-15 22:18:49 +0530

    Created OrderController

---
+++ src/main/java/lk/ijse/gdse67/evosellspringbackend/controller/OrderController.java
@@ -0,0 +1,11 @@
+package lk.ijse.gdse67.evosellspringbackend.controller;
+
+import org.springframework.data.convert.ReadingConverter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "api/v1/order")
+public class OrderController {
+
+}

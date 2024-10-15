969294b6328a49bdfe6557f77d1d60c523392884
Kavindu <gayanthakavindu3@gmail.com>
2024-10-16 00:58:04 +0530

    Created OrderDao

---
+++ src/main/java/lk/ijse/gdse67/evosellspringbackend/dao/OrderDao.java
@@ -0,0 +1,6 @@
+package lk.ijse.gdse67.evosellspringbackend.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderDao extends JpaRepository<OrderEntity> {
+}

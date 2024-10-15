04045728935eb3126531e928eb69d7537c152e92
Kavindu <gayanthakavindu3@gmail.com>
2024-10-16 00:58:53 +0530

    Added jpa repository

--- src/main/java/lk/ijse/gdse67/evosellspringbackend/dao/OrderDao.java
+++ src/main/java/lk/ijse/gdse67/evosellspringbackend/dao/OrderDao.java
@@ -1,6 +1,7 @@
 package lk.ijse.gdse67.evosellspringbackend.dao;
 
+import lk.ijse.gdse67.evosellspringbackend.entity.impl.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderDao extends JpaRepository<OrderEntity> {
+public interface OrderDao extends JpaRepository<OrderEntity,String> {
 }
